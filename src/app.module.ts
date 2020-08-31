df1474e5e42f80144dfbabf7b9c0db1b0069500a
paul-ml <pauljoe.george@monstar-lab.com>
2020-08-31 10:52:53 +0900

    adding typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [TasksModule], //modules required by this module
+  imports: [TasksModule, TypeOrmModule.forRoot(typeOrmConfig)], //modules required by this module
   controllers: [], //controllers required by this module
   providers: [],  // Array of providers, services come here
   exports: [] //Array of services which can be accessed from other modules
